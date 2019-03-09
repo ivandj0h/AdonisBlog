da89658ca715e8483d81f4e486c54fd4ef78849d
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-10 00:14:54 +0700

    modified package.json, create route & view for testing

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,10 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.on('/').render('home')
+
+// Testing Route
+// Route.get('/api/:id', function ({params}) {
+//     return `<p>ini adalah ${params.id}</p>`
+// })
+// Route.get('/contoh', () => { return `Haii` })
