f10d10f8ef40f8e6b8dbafc009af8772d4b50533
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-10 04:33:33 +0700

    modified main.edge, added update & destroy function on post controller

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -79,6 +79,40 @@ class PostController {
           post: post
         })
     }
+
+    async update({ params, request, response, session }) {
+        // Validate input
+        const validation = await validate(request.all(), {
+          title: 'required|min:3|max:255',
+          body: 'required|min:3'
+        })
+    
+        if(validation.fails()){
+          session.withErrors(validation.messages()).flashAll()
+          return response.redirect('back')
+        }
+    
+        const post = await Post.find(params.id)
+    
+        post.title = request.input('title')
+        post.body = request.input('body')
+    
+        await post.save()
+    
+        session.flash({ notification: 'Post Updated!' })
+    
+        return response.redirect('/posts')
+    }
+
+      async destroy({ params, session, response }) {
+        const post = await Post.find(params.id)
+    
+        await post.delete()
+    
+        session.flash({ notification: 'Post Deleted!' })
+        
+        return response.redirect('/posts')
+    }
 }
 
 module.exports = PostController
